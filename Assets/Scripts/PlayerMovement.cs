fa712e2f102f1caf6efc58fccfb226709998fee2
ferranclapes <114926403+ferranclapes@users.noreply.github.com>
2024-08-30 07:00:23 +0200

    Dragg option for the movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,9 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
 
+    //To change between movement types
+    public bool dragg = false;
+
     [SerializeField] private float speed = 5;
     private Vector2 movement;
     private Rigidbody2D rb;
@@ -15,7 +18,16 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+        if(!dragg){
+            //Movement type 1: No dragg, the player start at 100% of the speed and stops right when the button is released
+            rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+        }
+        else{ 
+            //Movement type 2: Dragg, the player slows down when no input is given
+            if(movement.x != 0 || movement.y != 0){
+                rb.linearVelocity = movement * speed;
+            }
+        }
     }
 
     private void OnMove (InputValue value) {
