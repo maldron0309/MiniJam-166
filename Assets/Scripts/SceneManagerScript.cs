183f5bd17475db1018410a8c2cc7744a8b2eacc7
ferranclapes <114926403+ferranclapes@users.noreply.github.com>
2024-08-30 21:27:36 +0200

    New space ship and pause menu

--- Assets/Scripts/SceneManagerScript.cs
+++ Assets/Scripts/SceneManagerScript.cs
@@ -3,7 +3,31 @@ using UnityEngine.SceneManagement;
 
 public class SceneManagerScript : MonoBehaviour
 {
+    public GameObject canvas;
+
     public void GoToPlayScene(){
         SceneManager.LoadScene("Ferran Trial");
+    }
+
+    public void GoToMainMenu(){
+
+    }
+
+    public void RestartGame(){
+        SceneManager.LoadScene("Ferran Trial");
+    }
+
+    public void ContinueGame(){
+        OnPause();
+    }
+
+    private void OnPause(){
+        if(canvas.activeInHierarchy){
+            canvas.SetActive(false);
+            Time.timeScale = 1;
+        }else{
+            canvas.SetActive(true);
+            Time.timeScale = 0;
+        }
     }
 }
