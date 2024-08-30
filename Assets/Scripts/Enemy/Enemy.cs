a0b4adc4dfa93a564b97ab0be9ad803d79ff4d55
ferranclapes <114926403+ferranclapes@users.noreply.github.com>
2024-08-30 08:36:27 +0200

    Enemies life bar
    The life of the enemies is displayed in a live bar on top of them

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -4,6 +4,12 @@ public class Enemy : MonoBehaviour
 {
     public float speed = 2f;
     [SerializeField] private int life = 3;
+    [SerializeField] private Transform lifebarFilling;
+    private int maxHealth;
+
+    void Start(){
+        maxHealth = life;
+    }
 
     void Update()
     {
@@ -17,6 +23,8 @@ public class Enemy : MonoBehaviour
 
     public void GetDamage(int damage){
         life -= damage;
+        float healthPercent = (float)life / maxHealth;
+        lifebarFilling.localScale = new Vector2(healthPercent, lifebarFilling.localScale.y);
         if(life <= 0){
             Destroy(gameObject);
         }
